0fdc84288b47e8b3482efe8a4602feb81ca3e8b2
harjeevanmaan <harjeevanm@gmail.com>
2020-02-13 15:43:24 -0500

    updated

--- Steering/steering.py
+++ Steering/steering.py
@@ -3,40 +3,24 @@
 import rospy
 import std_msgs
 import motor
-import argparse
 import time
+import threading
+import sys
 
+flag = False
 
-                        #add other parameters later
-parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--address", help="address of motor driver connection")
-parser.add_argument("-po", "--port", help="port of motor driver connection")
-parser.add_argument("-p", "--P", help="P value of PID controller")
-parser.add_argument("-i", "--I", help="I value of PID controller")
-parser.add_argument("-d", "--D", help="D value of PID controller")
-
-args = parser.parse_args()
-
-address = args.address
-port = int(args.port)
-p = float(args.P)
-i = float(args.I)
-d = float(args.D)
-
-
-def init_motor():     #add other parameters later
+def init_motor(steeringMotor, p, i, d):     #add other parameters later
     steeringMotor.connect()
 
-    steeringMotor.setCurrentLimit(4000)
+    steeringMotor.setCurrentLimit(6000)
     steeringMotor.windUpGaurd(1500)
     steeringMotor.setEncoderTicks(735*4)
     steeringMotor.setControlMode(2)
     steeringMotor.setWheelDiameter(.4)
 
-    #-- important infos --#
-    steeringMotor.setPid(p, i, d) #0.2, 0.2, 0
-    steeringMotor.setAcceleration(6)
-    steeringMotor.setDeceleration(6)
+    steeringMotor.setPid(0.4, 0.4, 0.1) #0.2, 0.2, 0
+    steeringMotor.setAcceleration(12)
+    steeringMotor.setDeceleration(12)
 
 
 def ticks_to_angle(ticks):
@@ -48,33 +32,32 @@ def ticks_to_angle(ticks):
 def angle_to_ticks(angle):          
     return (10000/52.5)*angle
 
-def set_tick(desired_ticks):
+def set_tick():
     
-    if(desired_ticks > 10000 or desired_ticks < -10000):
-        print("Invalid input, angle range is from +50 to -50 degrees")
-        exit()
-
-    current_tick = steeringMotor.currentEncoderTicks()
-    
-
     try:
-        if(desired_ticks > current_tick):
-            time.sleep(0.01)            #add delay for pcb communication time
-            steeringMotor.requestTickVelocity(-1000)
+        while True:
+            if(desired_ticks > 10000 or desired_ticks < -10000):
+                print("Invalid input, angle range is from +50 to -50 degrees")
+                continue
             time.sleep(0.01)
-            while(desired_ticks > steeringMotor.currentEncoderTicks()):
+            current_tick = steeringMotor.currentEncoderTicks()
+            
+            if(desired_ticks > current_tick+100):
+                time.sleep(0.01)            #add delay for pcb communication time
+                steeringMotor.requestTickVelocity(-1000)
                 time.sleep(0.01)
                 print("desired ticks {0}\t {1} \tpositive".format(desired_ticks,steeringMotor.currentEncoderTicks()))
-                time.sleep(0.01)
 
-        elif(desired_ticks < current_tick):
-            time.sleep(0.01)
-            steeringMotor.requestTickVelocity(1000)
-            time.sleep(0.01)
-            while(desired_ticks < steeringMotor.currentEncoderTicks()):
+            elif(desired_ticks < current_tick-100):
+                time.sleep(0.01)
+                steeringMotor.requestTickVelocity(1000)
                 time.sleep(0.01)
                 print("desired ticks {0}\t {1} \tnegative".format(desired_ticks,steeringMotor.currentEncoderTicks()))
+            else:
+                flag = False
                 time.sleep(0.01)
+                steeringMotor.requestTickVelocity(0)
+                print("Reached desired angle")
 
 
     finally:
@@ -91,23 +74,39 @@ def callback(data):
     print("init angle {0}".format(init_angle))
     pub.publish(init_angle)
 
+    global desired_ticks 
     desired_ticks = angle_to_ticks(data.data)
     print("desired ticks {0}".format(desired_ticks)) 
-    set_tick(desired_ticks)
-
 
 def listener():
     rospy.Subscriber("desired_angle", std_msgs.msg.Float32, callback)
     rospy.spin()
 
 def main():
-    rospy.init_node('steering_node', anonymous=True)
     
-    init_motor()
-    print("Init position {0}".format(steeringMotor.currentEncoderTicks()))
+    rospy.init_node('steering_node', anonymous=True)
+
+    address = rospy.get_param("~address")
+    port = rospy.get_param("~port")
+    p = rospy.get_param("~p")
+    i = rospy.get_param("~i")
+    d = rospy.get_param("~d")
+    
+    global steeringMotor
+    steeringMotor = motor.nextEng(address, port)
+    
+    init_motor(steeringMotor, p, i, d)
+    init_position = steeringMotor.currentEncoderTicks()
+    print("Init position {0}".format(init_position))
+
+    global desired_ticks
+    desired_ticks = init_position
+    set_tick_thread = threading.Thread(target=set_tick, args=())
+    set_tick_thread.daemon = True
+    set_tick_thread.start()
 
     listener()
 
 if __name__ == "__main__":
-    steeringMotor = motor.nextEng(address, port)
+    
     main()
